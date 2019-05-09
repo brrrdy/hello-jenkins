cb931fbad8f55c1fd5fce60a0c651decf22304ba
brrrdy <4109747+brrrdy@users.noreply.github.com>
2019-05-09 12:18:59 -0700

    Added deploy script

--- test/test.js
+++ test/test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const app = require('../app.js');
 
 describe('GET /', function() {
-    it('respond with hi world', function(done) {
+    it('respond with yo yo yo JENKINS', function(done) {
         request(app)
             .get('/')
             .expect('yo yo yo JENKINS', done);
